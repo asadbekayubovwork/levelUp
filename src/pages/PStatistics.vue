c2f88160784edc5b6417c68ab3f5ce9dcbeb77a5
asadbekayubovwork <asadbek.ayubov@webase.uz>
2026-02-06 21:32:19 +0500

    feat: Add product statistics page with product price bar chart visualization using Chart.js.

--- src/pages/PStatistics.vue
+++ src/pages/PStatistics.vue
@@ -1,9 +1,64 @@
-<template>
-    <div>
-        <h1>Statistika</h1>
-    </div>
-</template>
+<script setup>
+import { ref, onMounted, markRaw, onUnmounted } from 'vue'
+import Chart from 'chart.js/auto'
+import { productService } from '@/shared/api/productService'
+import { useThrottle } from '@/shared/lib/composables/useThrottle'
 
-<script setup lang="ts">
+const canvasRef = ref(null)
+const chartInstance = ref(null)
+const chartData = ref(null)
+
+const fetchData = async () => {
+    try {
+        const data = await productService.getProducts(10)
+        chartData.value = data
+        drawChart()
+    } catch (error) {
+        console.error('Error fetching statistics:', error)
+    }
+}
+
+const drawChart = () => {
+    if (!canvasRef.value || !chartData.value) return;
+
+    if (chartInstance.value) {
+        chartInstance.value.destroy()
+    }
+
+    const ctx = canvasRef.value.getContext('2d')
+
+    chartInstance.value = markRaw(new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: chartData.value.products.map(p => p.title),
+            datasets: [{
+                label: 'Price',
+                data: chartData.value.products.map(p => p.price),
+                backgroundColor: '#18A058'
+            }]
+        },
+        options: {
+            responsive: true,
+            maintainAspectRatio: false
+        }
+    }))
+}
+
+const throttledResize = useThrottle(3000, drawChart)
+window.addEventListener('resize', throttledResize)
+
+onMounted(fetchData)
+onUnmounted(() => {
+    window.removeEventListener('resize', throttledResize)
+})
 
 </script>
+
+<template>
+    <div class="p-4 w-full h-[600px]">
+        <h2 class="text-2xl font-medium mb-10">Mahsulotlar narxlari statistikasi</h2>
+        <div class="w-[70%] mx-auto relative h-full">
+            <canvas ref="canvasRef"></canvas>
+        </div>
+    </div>
+</template>
